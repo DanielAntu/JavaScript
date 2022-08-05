36ddcb5e5c35124fd50fe970a5b85eee19109d58
DanielAntunes <antunesdaniel083@gmail.com>
2022-08-05 17:18:08 -0300

    Atualização

--- exe018/script.js
+++ exe018/script.js
@@ -30,4 +30,33 @@ function adicionar() {
     }
     num.value = ''
     num.focus()
+    res.innerHTML = ''
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Por favor digite um valor')
+    } else {
+        let tot = valores.length
+        let maior =Number(valores[0])
+        let menor = Number(valores[0])
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += Number(valores[pos])
+            if (Number(valores[pos]) > maior) {
+                maior = valores[pos]
+            }
+            if (Number(valores[pos]) < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior número informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        res.innerHTML += `A média do valores é ${media}`
+    }
 }
